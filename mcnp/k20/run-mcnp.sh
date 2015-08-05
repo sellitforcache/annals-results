c10a1109607e53c78cae5ad4cef456b38f064952
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2015-08-05 16:40:12 +0200

    done w mcnp?

--- mcnp/k20/run-mcnp.sh
+++ mcnp/k20/run-mcnp.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 assembly-lw
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 pincell
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 jezebel
-
+ -omp 48 assembly-lw
+ -omp 48 pincell
+ -omp 48 jezebel
+ -omp 48 homfuel
