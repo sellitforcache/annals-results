467f56b34f2aef7ac867f8847172948d1b17304e
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2015-08-05 14:23:23 +0200

    adding mcnp inputs

---
+++ mcnp/titan/run-mcnp.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+/home/rmb/serpent/build2/serpent2/sss2 -omp 48 assembly-lw
+/home/rmb/serpent/build2/serpent2/sss2 -omp 48 pincell
+/home/rmb/serpent/build2/serpent2/sss2 -omp 48 jezebel
+/home/rmb/serpent/build2/serpent2/sss2 -omp 48 homfuel
