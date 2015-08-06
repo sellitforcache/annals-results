b6aacd31b233219b7c54001abed46a98a4578164
Ryan M. Bergmann <ryan.bergmann@psi.ch>
2015-08-06 04:38:44 -0700

    redid serpent sizes

--- serpent/titan/run-serp.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 assembly-lw
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 pincell
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 jezebel
-/home/rmb/serpent/build2/serpent2/sss2 -omp 48 homfuel
